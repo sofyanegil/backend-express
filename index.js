1feb7a0cc7d1c01c9d979fde60b11cb4a8f45d7d
Sofyan Egi <sofyanegil@gmail.com>
2024-06-08 10:52:46 +0700

    add cors and body parser

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-//import express
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 //init app
 const app = express();
+
+//use cors
+app.use(cors());
+
+//use body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 //define port
 const port = 3000;
