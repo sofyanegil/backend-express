473ebc1467edb741237b0f0413bfb4cd479e2662
Sofyan Egi <sofyanegil@gmail.com>
2024-06-20 08:03:59 +0700

    add user function

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const verifyToken = require('../middlewares/auth');
+
 const registerController = require('../controllers/RegisterController');
 const loginController = require('../controllers/LoginController');
+const userController = require('../controllers/UserController');
 
 //import validate
 const { validateRegister } = require('../utils/validators/auth');
 const { validateLogin } = require('../utils/validators/auth');
+const { validateUser } = require('../utils/validators/user');
 
 router.post('/register', validateRegister, registerController.register);
 router.post('/login', validateLogin, loginController.login);
+router.get('/admin/users', verifyToken, userController.findUsers);
+router.post('/admin/users', verifyToken, validateUser, userController.createUser);
+router.get('/admin/users/:id', verifyToken, userController.findUserById);
+router.put('/admin/users/:id', verifyToken, validateUser, userController.updateUser);
+router.delete('/admin/users/:id', verifyToken, userController.deleteUser);
 
 module.exports = router;
